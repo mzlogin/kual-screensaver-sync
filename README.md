021be24f6ed9bb5333027acaebcf7c3bbdae66ec
Zhuang Ma <chumpma@gmail.com>
2021-07-25 15:01:46 +0800

    release v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # kual-screensaver-sync
+
 依赖于 KUAL 的 Kindle 插件，用于从 GitHub 同步屏保图片。
+
+同步逻辑：
+
+1. 删除本地有，远程没有的屏保；
+2. 下载远程有，本地没有的屏保；
+3. 如果一张屏保本地和远程的 md5 值不一样，本地文件将被远程文件覆盖。
+
+## 安装方法
+
+该插件依赖于 KUAL，请确保之前已经正确安装它。
+
+本插件安装方法：
+
+1. 下载本项目代码，可以用 git clone，也可以到 [releases](https://github.com/mzlogin/kual-screensaver-syn/releases) 下载；
+
+2. 将 Kindle 用数据线连接电脑，把上面下载的代码里的 screensaver-sync 文件夹拷贝到 Kindle 的 /mnt/us/extensions 目录下。
+
+## 使用方法
+
+1. 打开 KUAL，就可以在插件列表里看到「ScreenSaver Sync」菜单项了；
+
+2. 点击后可进入插件功能列表；
+
+3. 点击「Start Sync」可以开始同步。
+
+## 适用机型
+
+当前只在 Kindle PaperWhite 一代上运行过，其它机型未验证。
+
+- Kindle PaperWhite
+
+## License
+
+[MIT License](./LICENSE)
---
+++ screensaver-sync/main.py
@@ -0,0 +1,93 @@
+import requests
+import os
+import hashlib
+
+base_url = 'https://cdn.jsdelivr.net/gh/mzlogin/kindle-paperwhite-screensavers@main/'
+screensaver_path = '/mnt/us/linkss/screensavers/'
+log_row_begin = 10
+log_row_end = 30
+log_row = 10
+
+def show_msg(text):
+    global log_row
+    print(text)
+    log_row += 1
+    if log_row > log_row_end:
+        log_row = log_row_begin
+    os.system('eips 10 %d "%s"' % (log_row, text))
+
+def get_file_md5(file_path):
+    with open(file_path, 'rb') as f:
+        md5obj = hashlib.md5()
+        md5obj.update(f.read())
+        _hash = md5obj.hexdigest()
+        return str(_hash).lower()
+
+def fetch_remote_data():
+    meta_url = base_url + 'meta.json'
+    resp = requests.get(meta_url)
+    if resp.status_code == 200:
+        return resp.json()
+    else:
+        return {}
+
+def fetch_local_data():
+    file_md5_dict = {}
+    for dir_path, dir_names, filenames in os.walk(screensaver_path):
+        for filename in filenames:
+            if os.path.splitext(filename)[1] == '.png':
+                file_path = os.path.join(screensaver_path, filename)
+                file_md5_dict[filename] = get_file_md5(file_path)
+    return file_md5_dict
+
+def compare_local_remote(local_data, remote_data):
+    local_unique_keys = local_data.keys() - remote_data.keys()
+    remote_unique_keys = remote_data.keys() - local_data.keys()
+    common_keys = local_data.keys() & remote_data.keys()
+    diff_keys = []
+    for key in common_keys:
+        if local_data[key] != remote_data[key]:
+            diff_keys.append(key)
+
+    return local_unique_keys , remote_unique_keys , diff_keys
+
+def process_local_unique(local_unique_keys):
+    for key in local_unique_keys:
+        file_path = '%s%s' % (screensaver_path, key)
+        show_msg('remove ' + key)
+        os.remove(file_path)
+
+def process_remote_unique(remote_unique_keys):
+    for key in remote_unique_keys:
+        remote_file_url = '%sscreensavers/%s' % (base_url, key)
+        file_path = '%s%s' % (screensaver_path, key)
+        resp = requests.get(remote_file_url)
+        show_msg('download ' + key)
+        with open(file_path, 'wb') as f:
+            f.write(resp.content)
+
+def process_diff(diff_keys):
+    process_local_unique(diff_keys)
+    process_remote_unique(diff_keys)
+
+def process():
+    show_msg('process start')
+    local_data = fetch_local_data()
+    show_msg('local keys: %d' % len(local_data))
+    remote_data = fetch_remote_data()
+    if len(remote_data) == 0:
+        show_msg('fetch_remote_data failed')
+        return
+    else:
+        show_msg('remote keys: %d' % len(remote_data))
+        local_unique_keys, remote_unique_keys, diff_keys = compare_local_remote(local_data, remote_data)
+        show_msg('local unique keys: %d' % len(local_unique_keys))
+        process_local_unique(local_unique_keys)
+        show_msg('remote unique keys: %d' % len(remote_unique_keys))
+        process_remote_unique(remote_unique_keys)
+        show_msg('diff keys: %d' % len(diff_keys))
+        process_diff(diff_keys)
+        show_msg('process finished')
+
+if __name__ == '__main__':
+    process()
